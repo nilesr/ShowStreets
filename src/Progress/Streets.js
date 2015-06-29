bb70115630d884403dd9ba6b4a022d5e0debf1af
Niles Rogoff <nilesrogoff@gmail.com>
2015-06-29 13:33:46 -0400

    Added temporary server side code

--- src/Progress/Streets.js
+++ src/Progress/Streets.js
@@ -1,94 +1,100 @@
 var Progress = Progress || {};
 
 Progress.streets = (function Streets($, L) {
-  var self = {};
+	var self = {};
 
-  function _init() {
-    // Styling: http://leafletjs.com/examples/geojson.html
-    // http://leafletjs.com/reference.html#path-dasharray
-    // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-dasharray
-    var mystyle = {
-      color: "#ccc",
-      weight: 2,
-      opacity: 0.75
-    };
+	function _init() {
+		// Styling: http://leafletjs.com/examples/geojson.html
+		// http://leafletjs.com/reference.html#path-dasharray
+		// https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-dasharray
+		var mystyle = {
+			color: "#ccc",
+			weight: 2,
+			opacity: 0.75
+		};
 
-    function onEachFeature(feature, layer) {
-      // does this feature have a property named popupContent?
-      if (feature.properties && feature.properties.type) {
-        // http://gis.stackexchange.com/questions/31951/how-to-show-a-popup-on-mouse-over-not-on-click
-        console.log("<a href=\"/audit/".concat(feature.properties.type,"/",feature.id,"\">Audit</a>"));
-        try {
-          layer.bindPopup("<a href=\"/audit/".concat(feature.properties.type,"/",feature.id,"\">Audit</a>"));
-        }
-        catch(e) {
-          layer.bindPopup("Failed")
-        }
-        // layer.on('mouseover', function (e) {
-        //   this.openPopup();
-        // });
-        // layer.on('mouseout', function (e) {
-        //   this.closePopup();
-        // });
-      }
-    }
+		function onEachFeature(feature, layer) {
+			// does this feature have a property named popupContent?
+			if (feature.properties && feature.properties.type) {
+				// http://gis.stackexchange.com/questions/31951/how-to-show-a-popup-on-mouse-over-not-on-click
+				try {
+					num = $.get("/audit_stats/total_audits/".concat(feature.id), function(data) { 
+						if (data.trim() == "1") {
+						  var plural = "person has";
+						} else {
+						  var plural = "people have";
+						}
+						layer.bindPopup(data.concat(" ", plural, " audited this. ","<a href=\"/audit/",feature.properties.type,"/",feature.id, feature.geometry /* <-- FIXME */ ,"\">Audit</a>")); 
+					});
+				}
+				catch(e) {
+					layer.bindPopup(e.message);
+				}
+				// layer.on('mouseover', function (e) {
+				//	 this.openPopup();
+				// });
+				// layer.on('mouseout', function (e) {
+				//	 this.closePopup();
+				// });
+			}
+		}
 
-    // Show streets
-    $.getJSON("resources/SmallMap_02_Streets.geojson", function(data) {
-      L.geoJson(data, {
-      pointToLayer: L.mapbox.marker.style,
-      style: function(feature) {
-        console.log(feature.properties.type);
-        var tempStyle = $.extend(true, {}, mystyle);
-        var randomInt = Math.floor(Math.random() * 5);
+		// Show streets
+		$.getJSON("resources/SmallMap_02_Streets.geojson", function(data) {
+			L.geoJson(data, {
+			pointToLayer: L.mapbox.marker.style,
+			style: function(feature) {
+				//console.log(feature.properties.type);
+				var tempStyle = $.extend(true, {}, mystyle);
+				var randomInt = Math.floor(Math.random() * 5);
 
-        tempStyle.color = Color.Pallet.sequential(randomInt);
-        tempStyle.opacity = 0.75;
-        tempStyle.weight = 3;
+				tempStyle.color = Color.Pallet.sequential(randomInt);
+				tempStyle.opacity = 0.75;
+				tempStyle.weight = 3;
 
-        return tempStyle;
-      },
-      onEachFeature: onEachFeature
-      })
-      .addTo(Progress.map);
-    })
-    .fail(function (result) {
-      console.log("Failed on: ", result);
-    });
+				return tempStyle;
+			},
+			onEachFeature: onEachFeature
+			})
+			.addTo(Progress.map);
+		})
+		.fail(function (result) {
+			console.log("Failed on: ", result);
+		});
 
-    // Show sidewalks
-    $.getJSON("resources/SmallMap_02_Sidewalks.geojson", function(data) {
-      L.geoJson(data, {
-        pointToLayer: L.mapbox.marker.style,
-        style: function(feature) {
-          // console.log(feature.properties.type);
-          var tempStyle = $.extend(true, {}, mystyle);
-          var randomInt = Math.floor(Math.random() * 5);
+		// Show sidewalks
+		$.getJSON("resources/SmallMap_02_Sidewalks.geojson", function(data) {
+			L.geoJson(data, {
+				pointToLayer: L.mapbox.marker.style,
+				style: function(feature) {
+					// console.log(feature.properties.type);
+					var tempStyle = $.extend(true, {}, mystyle);
+					var randomInt = Math.floor(Math.random() * 5);
 
-          //console.log(feature);
-          switch (feature.properties.type) {
-            case "footway" :
-              tempStyle.color = "#ccc";
-              tempStyle.dashArray = "3, 2"
-              break;
-            case "crosswalk":
-              tempStyle.color = "#888";
-              tempStyle.opacity = 0.25;
-              break;
-          }
+					//console.log(feature);
+					switch (feature.properties.type) {
+						case "footway" :
+							tempStyle.color = "#ccc";
+							tempStyle.dashArray = "3, 2"
+							break;
+						case "crosswalk":
+							tempStyle.color = "#888";
+							tempStyle.opacity = 0.25;
+							break;
+					}
 
-          return tempStyle;
-        },
-        onEachFeature: onEachFeature
-      })
-      .addTo(Progress.map);
-    })
-    .fail(function (result) {
-      console.log(result);
-    });
-  }
-  // Public methods
-  self.init = _init;
+					return tempStyle;
+				},
+				onEachFeature: onEachFeature
+			})
+			.addTo(Progress.map);
+		})
+		.fail(function (result) {
+			console.log(result);
+		});
+	}
+	// Public methods
+	self.init = _init;
 
-  return self;
+	return self;
 }($, L))
