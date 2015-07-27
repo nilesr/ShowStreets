3c83f41d71dfce69ee01640bf3b14fc180d48ef1
Niles Rogoff <nilesrogoff@gmail.com>
2015-07-27 15:20:59 -0400

    Fog-ish
    Just don’t zoom in too far

--- src/Progress/Streets_personal.js
+++ src/Progress/Streets_personal.js
@@ -1,4 +1,10 @@
 var Progress = Progress || {};
+var angleTo = function(pa, pb) {
+	return Math.atan((pb[1] - pa[1])/(pb[0] - pa[0]));
+}
+var distance = function(pa, pb) {
+	return Math.abs(Math.sqrt(Math.pow(pb[1]-pa[1], 2)+Math.pow(pb[0]-pa[0], 2)));
+}
 Progress.streets = (function Streets($, L) {
 	var self = {};
 
@@ -38,16 +44,39 @@ Progress.streets = (function Streets($, L) {
 			streetmap.addTo(Progress.map);
 			var points = [];
 			var squares = [];
+			var distance_between_points = .00075;
 			for (var i = 0; i < data["features"].length; i++) {
 				for (var s = 0; s < data["features"][i]["geometry"]["coordinates"].length; s++) {
 					if (data["features"][i]["geometry"]["coordinates"][s]) { // This is necessary because some of the points are null. Dunno why
-						points.push(data["features"][i]["geometry"]["coordinates"][s]);
+						// These are just to make code easier to write, they will not be changed
+						var pa = data["features"][i]["geometry"]["coordinates"][s]
+						var pb = data["features"][i]["geometry"]["coordinates"][s+1]
+						points.push(pa);
+						if (pb && distance(pa, pb) > distance_between_points) {
+							for (var k = 0; k < ( distance(pa, pb) / distance_between_points ) - 1; k += distance_between_points) {
+								points.push([pa[0] + (distance_between_points * Math.cos(angleTo(pa, pb))), pa[1] + (distance_between_points * Math.sin(angleTo(pa, pb)))]);
+							}
+						}
 					}
 				}
 			}
-			//var squarewidth = 0.0015;
-			var squarewidth = 0.0011;
+			//var radius = 0.0015;
+			var radius = 0.0008;
 			for (var i = 0; i < points.length; i++) {
+				/*var coords = [
+					[[points[i][0] - radius, points[i][1] - radius],
+					[points[i][0] + radius, points[i][1] - radius],
+					[points[i][0] + radius, points[i][1] + radius],
+					[points[i][0] - radius, points[i][1] + radius],
+					[points[i][0] - radius, points[i][1] - radius]]
+				];*/
+				var coords = [[]];
+				var complexity_constant = 20;
+				var vertical_stretch_factor = 0.8;
+				for (var j = 0; j < complexity_constant; j++) {
+					coords[0].push([points[i][0] + radius * Math.sin(2 * Math.PI * j/complexity_constant), points[i][1] + vertical_stretch_factor * radius * Math.cos(2 * Math.PI * j/complexity_constant)]);
+				}
+				coords[0].push(coords[0][0]);
 				squares.push({
 					"type": "Feature",
 					"properties": {
@@ -55,13 +84,7 @@ Progress.streets = (function Streets($, L) {
 					},
 					"geometry": {
 					"type": "Polygon",
-					"coordinates": [
-							[[points[i][0] - squarewidth, points[i][1] - squarewidth],
-							[points[i][0] + squarewidth, points[i][1] - squarewidth],
-							[points[i][0] + squarewidth, points[i][1] + squarewidth],
-							[points[i][0] - squarewidth, points[i][1] + squarewidth],
-							[points[i][0] - squarewidth, points[i][1] - squarewidth]]
-						]
+					"coordinates": coords
 					}
 				});
 			}
@@ -69,7 +92,8 @@ Progress.streets = (function Streets($, L) {
 				"type": "FeatureCollection",
 				"features": squares
 			}
-			squares3 = turf.merge(squares2);
+			squares3 = turf.merge(squares2, function(err, out) {alert(out);});
+			console.log(JSON.stringify(squares2, null));
 			squares3.geometry.coordinates.push([
 				[
 					180,
