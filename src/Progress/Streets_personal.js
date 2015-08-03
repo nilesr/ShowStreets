7c151e82c8e8174972054305a52dfa28826e9a84
Niles Rogoff <nilesrogoff@gmail.com>
2015-08-03 12:14:13 -0400

    Now loads fog on page load

--- src/Progress/Streets_personal.js
+++ src/Progress/Streets_personal.js
@@ -38,104 +38,100 @@ Progress.streets = (function Streets($, L) {
 				onEachFeature: onEachFeature
 			});
 			streetmap.addTo(Progress.map);
-			$("#load_fog").click(function() {
-				$("#load_fog_button").html("Loading...").addClass("pure-button-disabled"); // This isn't working :(
-				var points = [];
-				var squares = [];
-				// Even with a complexity_constant of 3 and a distance_between_points of .0008, it still takes ~30 seconds to load.
-				// With a complexity_constant of 128 and the same distance_between_points it takes 1:15 to load. 
-				var complexity_constant = 4; // Was 10
-				var vertical_stretch_factor = 0.8;
-				//var radius = 0.0015;
-				var radius = 0.0006;
-				//var distance_between_points = (1/vertical_stretch_factor)*radius/2 + .00001
-				var pairs = [];
-				var distance_between_points = radius;
-				for (var i = 0; i < data["features"].length; i++) {
-					for (var s = 0; s < data["features"][i]["geometry"]["coordinates"].length; s++) {
-						if (data["features"][i]["geometry"]["coordinates"][s]) { // This is necessary because some of the points are null. Dunno why
-							// These are just to make code easier to write, they will not be changed
-							var pa = data["features"][i]["geometry"]["coordinates"][s];
-							var pb = data["features"][i]["geometry"]["coordinates"][s+1]; // Because this might be null, we need the following while loop.
-							points.push(pa);
-							var a = 1;
-							while (pb === null) { // Can't just do while !pb because if pb is undefined the while loop will become infinite
-								a++;
-								pb = data["features"][i]["geometry"]["coordinates"][s+a];
+			var points = [];
+			var squares = [];
+			// Even with a complexity_constant of 3 and a distance_between_points of .0008, it still takes ~30 seconds to load.
+			// With a complexity_constant of 128 and the same distance_between_points it takes 1:15 to load. 
+			var complexity_constant = 4; // Was 10
+			var vertical_stretch_factor = 0.8;
+			//var radius = 0.0015;
+			var radius = 0.0006;
+			//var distance_between_points = (1/vertical_stretch_factor)*radius/2 + .00001
+			var pairs = [];
+			var distance_between_points = radius;
+			for (var i = 0; i < data["features"].length; i++) {
+				for (var s = 0; s < data["features"][i]["geometry"]["coordinates"].length; s++) {
+					if (data["features"][i]["geometry"]["coordinates"][s]) { // This is necessary because some of the points are null. Dunno why
+						// These are just to make code easier to write, they will not be changed
+						var pa = data["features"][i]["geometry"]["coordinates"][s];
+						var pb = data["features"][i]["geometry"]["coordinates"][s+1]; // Because this might be null, we need the following while loop.
+						points.push(pa);
+						var a = 1;
+						while (pb === null) { // Can't just do while !pb because if pb is undefined the while loop will become infinite
+							a++;
+							pb = data["features"][i]["geometry"]["coordinates"][s+a];
+						}
+						/*if (pb && distance(pa, pb) > distance_between_points) {
+							for (var k = 0; k < ( distance(pa, pb) / distance_between_points ) - 1; k += distance_between_points) {
+								points.push([pa[0] + (distance_between_points * Math.cos(angleTo(pa, pb))), pa[1] + (distance_between_points * Math.sin(angleTo(pa, pb)))]);
 							}
-							/*if (pb && distance(pa, pb) > distance_between_points) {
-								for (var k = 0; k < ( distance(pa, pb) / distance_between_points ) - 1; k += distance_between_points) {
-									points.push([pa[0] + (distance_between_points * Math.cos(angleTo(pa, pb))), pa[1] + (distance_between_points * Math.sin(angleTo(pa, pb)))]);
-								}
-							}*/
-							if (pb && distance(pa, pb) > radius) {
-								pairs.push([pa, pb]);
-							}
+						}*/
+						if (pb && distance(pa, pb) > radius) {
+							pairs.push([pa, pb]);
 						}
 					}
 				}
-				for (var i = 0; i < points.length; i++) {
-					var coords = [];
-					for (var j = 0; j < complexity_constant; j++) {
-						coords.push([points[i][0] + radius * Math.sin(2 * Math.PI * j/complexity_constant + (Math.PI / 4)), points[i][1] + vertical_stretch_factor * radius * Math.cos(2 * Math.PI * j/complexity_constant + (Math.PI / 4))]);
-						// The extra pi/4 is so that if you have a complexity_constant of 4, the squares are aligned east/west instead of 45° off.
-					}
-					coords.push(coords[0]);
-					squares.push(coords);
+			}
+			for (var i = 0; i < points.length; i++) {
+				var coords = [];
+				for (var j = 0; j < complexity_constant; j++) {
+					coords.push([points[i][0] + radius * Math.sin(2 * Math.PI * j/complexity_constant + (Math.PI / 4)), points[i][1] + vertical_stretch_factor * radius * Math.cos(2 * Math.PI * j/complexity_constant + (Math.PI / 4))]);
+					// The extra pi/4 is so that if you have a complexity_constant of 4, the squares are aligned east/west instead of 45° off.
 				}
-				for (var i = 0; i < pairs.length; i++) {
-					var pa = pairs[i][0];
-					var pb = pairs[i][1];
-					var coords = [];
-					var recip = -1 / angleTo(pa, pb);
-					coords.push([pa[0] + radius * Math.sin(recip), pa[1] + radius * Math.cos(recip)]);
-					coords.push([pa[0] - radius * Math.sin(recip), pa[1] - radius * Math.cos(recip)]);
-					coords.push([pb[0] - radius * Math.sin(recip), pb[1] - radius * Math.cos(recip)]);
-					coords.push([pb[0] + radius * Math.sin(recip), pb[1] + radius * Math.cos(recip)]);
-					coords.push(coords[0]);
-					squares.push(coords);
+				coords.push(coords[0]);
+				squares.push(coords);
+			}
+			for (var i = 0; i < pairs.length; i++) {
+				var pa = pairs[i][0];
+				var pb = pairs[i][1];
+				var coords = [];
+				var recip = -1 / angleTo(pa, pb);
+				coords.push([pa[0] + radius * Math.sin(recip), pa[1] + radius * Math.cos(recip)]);
+				coords.push([pa[0] - radius * Math.sin(recip), pa[1] - radius * Math.cos(recip)]);
+				coords.push([pb[0] - radius * Math.sin(recip), pb[1] - radius * Math.cos(recip)]);
+				coords.push([pb[0] + radius * Math.sin(recip), pb[1] + radius * Math.cos(recip)]);
+				coords.push(coords[0]);
+				squares.push(coords);
+			}
+			var reader = new jsts.io.WKTReader();
+			var parser = new jsts.io.GeoJSONParser();
+			var base = reader.read(polyToBullshit(squares[0]));
+			for (var i = 1; i < squares.length; i++) {
+				base = base.union(reader.read(polyToBullshit(squares[i])));
+			}
+			var squares2 = parser.write(base);
+			//console.log(JSON.stringify(squares2, null));
+			squares2.coordinates.push([
+				[
+					180,
+					-180
+				],
+				[
+					180,
+					180
+				],
+				[
+					-180,
+					180
+				],
+				[
+					-180,
+					-180
+				],
+				[
+					180,
+					-180
+				]
+			]);
+			var squares3 = {
+				"type": "FeatureCollection",
+				"features": [squares2]
+			};
+			L.geoJson(squares3, {
+				style: function(feature) {
+					return {fillColor: "#000000", fillOpacity: 0.5, clickable: false, lineJoin: "round", className: "squares3", color: "#000000"};
 				}
-				var reader = new jsts.io.WKTReader();
-				var parser = new jsts.io.GeoJSONParser();
-				var base = reader.read(polyToBullshit(squares[0]));
-				for (var i = 1; i < squares.length; i++) {
-					base = base.union(reader.read(polyToBullshit(squares[i])));
-				}
-				var squares2 = parser.write(base);
-				//console.log(JSON.stringify(squares2, null));
-				squares2.coordinates.push([
-					[
-						180,
-						-180
-					],
-					[
-						180,
-						180
-					],
-					[
-						-180,
-						180
-					],
-					[
-						-180,
-						-180
-					],
-					[
-						180,
-						-180
-					]
-				]);
-				var squares3 = {
-					"type": "FeatureCollection",
-					"features": [squares2]
-				};
-				L.geoJson(squares3, {
-					style: function(feature) {
-						return {fillColor: "#000000", fillOpacity: 0.5, clickable: false, lineJoin: "round", className: "squares3", color: "#000000"};
-					}
-				}).addTo(Progress.map);
-				$("#load_fog").remove();
-			});
+			}).addTo(Progress.map);
 			$("#loading").html("");
 		})
 		.fail(function (result) {
